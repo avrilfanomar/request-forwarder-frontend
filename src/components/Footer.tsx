ae8ab4e9fa326f4d6101375ed498479d49c98005
Andrii Andriichuk <andrey000mar@gmail.com>
2025-04-19 09:04:40 +0200

    add "report an issue" link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,12 +2,21 @@ import React from 'react';
 
 const Footer: React.FC = () => {
   const currentYear = new Date().getFullYear();
-  
+
   return (
     <footer>
       <div className="container">
         <p>
           &copy; {currentYear} Request Forwarder Web UI
+        </p>
+        <p className="mt-2">
+          <a 
+            href="https://github.com/avrilfanomar/request-forwarder-frontend/issues" 
+            target="_blank" 
+            rel="noopener noreferrer"
+          >
+            Report an Issue
+          </a>
         </p>
         <p className="mt-2">
           <small>This service requires a paid subscription for production use.</small>
